5952985b6d74a7f16028cfbe69bbcb8bf4440f2b
toshiya2771 <92073288+toshiya2771@users.noreply.github.com>
2021-12-15 16:46:56 +0900

    Update myled.c

--- myled.c
+++ myled.c
@@ -1,6 +1,6 @@
-//SPDX-License-Identifier: GPL-2.0
+//SPDX-License-Identifier: GPL-3.0
 /*
- * Copyright (C) 2011 STRATO, All rights reserved,
+ * Copyright (C) 2021 ryuichi ueda, All rights reserved,
  */
 
 #include <linux/module.h>
